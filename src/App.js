546d4de6d0b17fdd9844c5cfaa5c01381a06a778
geoni153 <63406193+geoni153@users.noreply.github.com>
2021-01-28 00:26:04 +0900

    dynamic component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from "react";
 
-function Food({fav}) {
-  return <h1>I like {fav} </h1>;
+function Food({name, picture}) {
+  return <div>
+    <h2>I like {name}</h2>
+    <img src={picture} />
+  </div>
 }
+
+const foodILike = [
+  {
+    name: "chicken",
+    image: "https://www.thespruceeats.com/thmb/PrfALhou8wczKx74j7cp_F6lvbs=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/korean-bbq-chicken-wings-2118689-hero-01-5c92a63b46e0fb000144278f.jpg"
+  },
+  {
+    name: "kimchi",
+    image: "https://www.koreanbapsang.com/wp-content/uploads/2019/11/DSC_0831.jpg"
+  }
+];
 
 function App() {
   return (
   <div> 
-    <h1>hello</h1>
-    <Food fav="chicken"/>
-    <Food fav="pizza"/>
-    <Food fav="ramen"/>
-    <Food fav="kimchi"/>
+    {foodILike.map(item => <Food name={item.name} picture={item.image} />)}
   </div>
   );
 }
