2aa4605495335a3566569eb894c4bdb35ae76a45
geoni153 <63406193+geoni153@users.noreply.github.com>
2021-01-27 23:26:54 +0900

    jsx with props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
-import Potato from "./Potato";
+
+function Food({fav}) {
+  return <h1>I like {fav} </h1>;
+}
 
 function App() {
   return (
   <div> 
     <h1>hello</h1>
-    <Potato />
+    <Food fav="chicken"/>
+    <Food fav="pizza"/>
+    <Food fav="ramen"/>
+    <Food fav="kimchi"/>
   </div>
   );
 }
--- src/Potato.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Potato(){
-    return (
-        <h3> I love potatoes.</h3>
-    );
-}
-
-export default Potato;
